b62a4336190668fa2fdf9cef9525adced21c507f
ChippyChoppy <rebeccamargaret13@gmail.com>
2022-02-25 18:15:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+      <h1>Call Congress</h1>
+      <p>this is call congress 2.0</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
